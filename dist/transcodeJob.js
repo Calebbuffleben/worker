f03d6c9b0ce3a11a4d707fad73471b7e7ac0682a
Caleb Buffleben <calebbuffleben@gmail.com>
2025-08-30 10:58:50 -0300

    updating code

--- dist/transcodeJob.js
+++ dist/transcodeJob.js
@@ -50,11 +50,11 @@ async function handleTranscodeJob(job) {
     const crf = data.crf ?? 21;
     const preset = data.preset ?? 'veryfast';
     const ladder = data.ladder ?? [
-        { width: 1920, height: 1080, videoBitrateKbps: 6000, audioBitrateKbps: 128 }, // 1080p
+        //  { width: 1920, height: 1080, videoBitrateKbps: 6000, audioBitrateKbps: 128 }, // 1080p
         { width: 1280, height: 720, videoBitrateKbps: 3000, audioBitrateKbps: 128 }, // 720p
-        { width: 854, height: 480, videoBitrateKbps: 1500, audioBitrateKbps: 128 }, // 480p
+        //  { width: 854, height: 480, videoBitrateKbps: 1500, audioBitrateKbps: 128 },   // 480p
         { width: 640, height: 360, videoBitrateKbps: 800, audioBitrateKbps: 128 }, // 360p
-        { width: 426, height: 240, videoBitrateKbps: 400, audioBitrateKbps: 128 }, // 240p
+        //  { width: 426, height: 240, videoBitrateKbps: 400, audioBitrateKbps: 128 },    // 240p
     ];
     const hlsPath = data.hlsPath ?? 'hls';
     return (0, ffmpeg_1.withTempDir)(async (tmp) => {
